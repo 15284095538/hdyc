4381609a3011359b3261011eb52dd5f4e95973d6
Leslie <921110219@qq.com>
2018-06-20 16:56:09 +0800

    TvT

--- pages/store/home/index.js
+++ pages/store/home/index.js
@@ -5,7 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    menu: [//导航
+      {
+        "path": "/images/store_menu_01.png",
+        "text": "美容保养"
+      },
+      {
+        "path": "/images/store_menu_02.png",
+        "text": "维修厂"
+      },
+      {
+        "path": "/images/store_menu_03.png",
+        "text": "洗车"
+      }],
+      list: [{
+        "path": "/images/store_menu_01.png",
+        "name": "邛崃市天和车王养护",
+        "grade": 0,
+        "address": "成都市崇州市老陈大路462号（汇蜀路口）",
+        "tag": ["美容","安装"],
+        "distance": "3.1KM"
+      }, {
+        "path": "/images/store_menu_01.png",
+        "name": "邛崃市天和车王养护",
+        "grade": 0,
+        "address": "成都市崇州市老陈大路462号（汇蜀路口）",
+        "tag": ["美容", "安装"],
+        "distance": "3.1KM"
+        }, {
+          "path": "/images/store_menu_01.png",
+          "name": "邛崃市天和车王养护",
+          "grade": 0,
+          "address": "成都市崇州市老陈大路462号（汇蜀路口）",
+          "tag": ["美容", "安装"],
+          "distance": "3.1KM"
+      }, {
+        "path": "/images/store_menu_01.png",
+        "name": "邛崃市天和车王养护",
+        "grade": 0,
+        "address": "成都市崇州市老陈大路462号（汇蜀路口）",
+        "tag": ["美容", "安装"],
+        "distance": "3.1KM"
+      }]
   },
 
   /**
