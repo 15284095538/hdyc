0bbc3d752012dd87855b4765c1dbe6a467214d67
zm <863535838@qq.com>
2018-06-22 12:06:15 +0800

    det

--- pages/index/details/details.js
+++ pages/index/details/details.js
@@ -13,5 +13,36 @@ Page({
         '/images/banner.jpg'
       ]
     },
+    pinglun: [
+      {
+        npic: '/images/car_03.png',
+        name: '李小姐',
+        flag: '4',
+        time: '2018-05-21',
+        message: '每次都在这里洗车，洗的非常专业，服务特别好，很用心。',
+        pic: [
+          '/images/car_03.png',
+          '/images/car_03.png',
+          '/images/car_03.png',
+          '/images/car_03.png',
+        ],
+        stype: '标准洗车'
+      },
+      {
+        npic: '/images/car_03.png',
+        name: '李小姐',
+        flag: '4',
+        time: '2018-05-21',
+        message: '每次都在这里洗车，洗的非常专业，服务特别好，很用心。',
+        pic: [
+          '/images/car_03.png',
+          '/images/car_03.png',
+          '/images/car_03.png',
+          '/images/car_03.png',
+        ],
+        stype: '标准洗车'
+      },
+
+    ]
   }
 })
