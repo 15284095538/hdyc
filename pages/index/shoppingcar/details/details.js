fa8e35b876143eeb5363a105fee37f1032f5fa3d
zm <863535838@qq.com>
2018-06-25 16:19:12 +0800

    pay

--- pages/index/shoppingcar/details/details.js
+++ pages/index/shoppingcar/details/details.js
@@ -1,66 +1,34 @@
-// pages/index/shoppingcar/details/details.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    swiper: {//banner图
+      indicatorDots: true,
+      autoplay: true,
+      interval: 3000,
+      duration: 500,
+      indicatorcolor: '#d5d5d5',
+      indicatoractivecolor: "#0084ff",
+      imgUrl: [
+        '/images/banner.jpg',
+        '/images/banner.jpg',
+        '/images/banner.jpg'
+      ]
+    },
+    layerColorDisplay:'none',
+    layerLiftcarDisplay: 'none',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  layerColorclick(e){
+    this.setData({ layerColorDisplay:'block' })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  bglayerColorclick(e){
+    this.setData({ layerColorDisplay: 'none' })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  closelayerColor(e){
+    this.bglayerColorclick();
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  bglayerLiftcarclick(e){
+    this.setData({ layerLiftcarDisplay: 'none' })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  layerLiftcarclick(e){
+    this.setData({ layerLiftcarDisplay: 'block' })
   }
 })
