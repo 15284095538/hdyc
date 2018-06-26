dfa21bac0db411b0f975b4dfce729f957d855d2f
Leslie <921110219@qq.com>
2018-06-26 16:22:25 +0800

    Lelsie

---
+++ pages/my/business/money_details/money_details.js
@@ -0,0 +1,86 @@
+// pages/my/business/money_details/money_details.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "list": [{
+      "path": "/images/store_menu_01.png",
+      "title": "提现",
+      "type": "0",
+      "time": "50-20 08:00",
+      "num": "+5432.10"
+    },
+    {
+      "path": "/images/store_menu_01.png",
+      "title": "转账",
+      "type": "1",
+      "time": "50-20 08:00",
+      "num": "-5432.10"
+    }, {
+      "path": "/images/store_menu_01.png",
+      "title": "转账",
+      "type": "1",
+      "time": "50-20 08:00",
+      "num": "+5432.10"
+    }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
