c6b2d06c265652032e4e146fd795c051f0abc1cb
zm <863535838@qq.com>
2018-06-26 10:52:54 +0800

    list

--- pages/index/washcar/list/list.js
+++ pages/index/washcar/list/list.js
@@ -1,66 +1,38 @@
-// pages/index/washcar/list/list.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    display: 'none',
+    layerid:0,
+    menu: [//导航
+      {
+        "path": "/images/xiala_normal@2x.png",
+        "selectpath": "/images/xiala_hl@2x.png",
+        "text": "区域选择",
+        "key": 0
+      }
+    ],
+    menulist: [//城市列表
+      {
+        "text": "成都市"
+      },
+      {
+        "text": "德阳"
+      },
+      {
+        "text": "青羊"
+      }
+    ],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-  
+    
   },
+  onLoad() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  listTopclick(e){
+    var menuid = e.currentTarget.dataset.id;
+    this.setData({ display: 'block', menuid: 0 })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  listToplayerLiclick(e){
+    this.setData({ display: 'none', menuid: 2 })
   }
 })
