2b66e85208ccdab5ad7a1adc166c6e3b83f90347
Leslie <921110219@qq.com>
2018-06-27 15:35:10 +0800

    Leslie

--- pages/my/business/order/order.js
+++ pages/my/business/order/order.js
@@ -14,41 +14,41 @@ Page({
     selectid: 0,
     ShowList: [],
     list1: [{
-        "order_ns": "TH28497737",
-        "path": "/images/store_menu_01.png",
-        "name": "3M高效发动机内部清洗剂204Ml",
-        "num": 3,
-        "price": 68.00,
-        "status": "已到店"
-      },
-      {
-        "order_ns": "TH28497737",
-        "path": "/images/store_menu_01.png",
-        "name": "3M高效发动机内部清洗剂204Ml",
-        "num": 4,
-        "price": 68.00,
-        "status": "已到店"
-      }
+      "order_ns": "TH28497737",
+      "path": "/images/store_menu_01.png",
+      "name": "3M高效发动机内部清洗剂204Ml",
+      "num": 3,
+      "price": 68.00,
+      "status": "已到店"
+    },
+    {
+      "order_ns": "TH28497737",
+      "path": "/images/store_menu_01.png",
+      "name": "3M高效发动机内部清洗剂204Ml",
+      "num": 4,
+      "price": 68.00,
+      "status": "已到店"
+    }
     ],
     list2: [{
-        "order_ns": "TH28497737",
-        "path": "/images/store_menu_01.png",
-        "name": "3M高效发动机内部清洗剂",
-        "num": 5,
-        "price": 68.00,
-        "status": "待付款"
-      },
-      {
-        "order_ns": "TH28497737",
-        "path": "/images/store_menu_01.png",
-        "name": "发动机内部清洗剂204Ml",
-        "num": 6,
-        "price": 68.00,
-        "status": "待评价"
-      }
+      "order_ns": "TH28497737",
+      "path": "/images/store_menu_01.png",
+      "name": "3M高效发动机内部清洗剂",
+      "num": 5,
+      "price": 68.00,
+      "status": "待付款"
+    },
+    {
+      "order_ns": "TH28497737",
+      "path": "/images/store_menu_01.png",
+      "name": "发动机内部清洗剂204Ml",
+      "num": 6,
+      "price": 68.00,
+      "status": "待评价"
+    }
     ]
   },
-  onLoad() {
+  onLoad(option) {
     wx.setNavigationBarTitle({
       title: '我的订单'
     });
@@ -56,6 +56,20 @@ Page({
     that.setData({
       ShowList: that.data.list1
     });
+    if (option.id) {
+      that.setData({
+        selectid: option.id
+      });
+      if (option.id == 0) {
+        that.setData({
+          ShowList: that.data.list1
+        });
+      } else {
+        that.setData({
+          ShowList: that.data.list2
+        });
+      }
+    };
   },
   ChangeSelect(e) {
     var that = this;
