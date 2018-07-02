e02558f47d1a0c3b3bf1150c333fa9a8e037df5a
zm <863535838@qq.com>
2018-07-02 14:18:29 +0800

    a

--- pages/index/home/index.js
+++ pages/index/home/index.js
@@ -12,48 +12,7 @@ Page({
       indicatoractivecolor: "#0084ff",
       imgUrl:[]
     },
-    menu:[//导航
-      {
-        "path":"/images/menu_10.png",
-        "text":"洗车",
-        "link":"/pages/index/list/list"
-      },
-      {
-        "path": "/images/menu_17.png",
-        "text": "养护推荐",
-        "link": "/pages/index/list/list"
-      },
-      {
-        "path": "/images/menu_05.png",
-        "text": "隐形车衣",
-        "link": "/pages/index/list/list"
-      },
-      {
-        "path": "/images/menu_07.png",
-        "text": "汽车除甲醛",
-        "link": "/pages/index/list/list"
-      },
-      {
-        "path": "/images/menu_19.png",
-        "text": "贴膜",
-        "link": "/pages/index/list/list"
-      },
-      {
-        "path": "/images/menu_16.png",
-        "text": "钣金喷漆",
-        "link": "/pages/index/list/list"
-      },
-      {
-        "path": "/images/menu_17.png",
-        "text": "全车镀晶",
-        "link": "/pages/index/list/list"
-      },
-      {
-        "path": "/images/menu_18.png",
-        "text": "车用品购买",
-        "link": "/pages/index/list/list"
-      }
-    ],
+    menu:'',//导航
     classify:'',
   },
   onLoad(){
@@ -112,9 +71,14 @@ Page({
         for (let i = 0; i < res.data.data.length; i++) {
           res.data.data[i].link = '/pages/index/list/list'
         }
+        res.data.data[0].link = '/pages/index/washcar/list/list';//洗车
+        res.data.data[5].link = '/pages/index/spraypaint/home/home';//钣金喷漆
+        res.data.data[7].link = '/pages/index/suppliescar/list/list';//车用品购买
         console.log(res)
         classifyS = true
-        
+        that.setData({
+          menu: res.data.data,
+        })
       }
     })
 
