5dba0f265cf017757871d818e21cd5814ca586a0
zm <863535838@qq.com>
2018-07-23 17:17:41 +0800

    dde

--- yfl3/application/index/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//配置文件
-return [
-    'view_replace_str' => [
-        '__CSS__'   => DS.'index'.DS.'css',
-        '__JS__'   => DS.'index'.DS.'js',
-        '__IMG__'   => DS.'index'.DS.'images',
-    ],
-];
